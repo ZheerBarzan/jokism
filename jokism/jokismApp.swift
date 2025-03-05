b2b8f03a0db70d3c3ae26fbc765c49d3370d7431
ZheerBarzan <zheerbarzan85@outlook.com>
2025-03-05 16:12:50 +0300

    Initial Commit

---
+++ jokism/jokismApp.swift
@@ -0,0 +1,17 @@
+//
+//  jokismApp.swift
+//  jokism
+//
+//  Created by zheer barzan on 5/3/25.
+//
+
+import SwiftUI
+
+@main
+struct jokismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ jokismTests/jokismTests.swift
@@ -0,0 +1,17 @@
+//
+//  jokismTests.swift
+//  jokismTests
+//
+//  Created by zheer barzan on 5/3/25.
+//
+
+import Testing
+@testable import jokism
+
+struct jokismTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
