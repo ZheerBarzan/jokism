1c3c1e269d8133573b8970f99444ded84b433d10
ZheerBarzan <zheerbarzan85@outlook.com>
2025-03-05 16:17:42 +0300

    the intial setup of the application

---
+++ jokism/Model/joke.swift
@@ -0,0 +1,14 @@
+//
+//  joke.swift
+//  jokism
+//
+//  Created by zheer barzan on 5/3/25.
+//
+
+import Foundation
+
+struct Joke : Codable, Identifiable{
+    var id: ObjectIdentifier
+    
+    
+}
