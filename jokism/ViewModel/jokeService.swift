875279081d7af56d391731c6512d1eaf748bbfc5
ZheerBarzan <zheerbarzan85@outlook.com>
2025-03-14 23:09:59 +0300

    adding all the API's and randomising the jokes from diffrent sources

--- jokism/ViewModel/jokeService.swift
+++ jokism/ViewModel/jokeService.swift
@@ -9,6 +9,40 @@ import Foundation
 
 class JokeService{
     
+    func fetchJoke() async throws -> Joke{
+        let sources = [fetchIcanHazDadJoke, fetchJokeAPI, fetchChuckNorriesJoke]
+        let randomSource = sources.randomElement()!
+        return try await randomSource()
+    }
+    
+    private func fetchIcanHazDadJoke() async throws -> Joke{
+        let url = URL(string: "https://icanhazdadjoke.com/")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        
+        let (data, _) = try await URLSession.shared.data(for: request)
+        let response = try JSONDecoder().decode(IcanHazDadJokeResponse.self, from: data)
+        
+        return Joke(id: response.id, content: response.joke)
+    }
+    
+    
+    private func fetchJokeAPI() async throws -> Joke{
+        let url = URL(string: "https://v2.jokeapi.dev/joke/Any?type=single")!
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let response = try JSONDecoder().decode(JokeAPIResponse.self, from: data)
+        
+        return Joke(id: String(response.id), content: response.joke)
+
+    }
+    private func fetchChuckNorriesJoke() async throws -> Joke{
+        let url = URL(string: "https://api.chucknorris.io/jokes/random")!
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let response = try JSONDecoder().decode(ChuckNorrisResponse.self, from: data)
+        
+        return Joke(id: response.id, content: response.joke)
+
+    }
     
 }
 
